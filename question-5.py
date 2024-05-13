dd4ea2dc32ab02ffc8fa02da7a02029a87fc4b9f
Suman-De18 <sumande191@gmail.com>
2024-05-13 20:54:36 +0530

    plot showing the time taken by the two DFT methods as a function of n

---
+++ question-5.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+def compute_dft(x):
+    N = len(x)
+    n = np.arange(N)
+    k = n.reshape((N, 1))
+    e = np.exp(-2j * np.pi * k * n / N)
+    X = np.dot(x, e)
+    return X
+
+
+import timeit
+
+def time_dft(n):
+    x = np.random.rand(n)
+    return timeit.timeit(lambda: compute_dft(x), number=1000)
+
+def time_fft(n):
+    x = np.random.rand(n)
+    return timeit.timeit(lambda: np.fft.fft(x), number=1000)
+
+# Measure time for different n values
+n_values = range(4, 101)
+dft_times = [time_dft(n) for n in n_values]
+fft_times = [time_fft(n) for n in n_values]
+
+# Make a plot
+import matplotlib.pyplot as plt
+plt.plot(n_values, dft_times, label="Direct Computation")
+plt.plot(n_values, fft_times, label="numpy.fft.fft")
+plt.xlabel("n")
+plt.ylabel("Execution Time (seconds)")
+plt.title("DFT Computation Time vs. n")
+plt.legend()
+plt.show()
